e88dda476d1153c57e9037a852aee015cf79f585
de1eteCode <skozlov2015@yandex.ru>
2021-10-17 22:19:22 +0500

    [Fix] Json сериализация на клиенте

--- MedicalLaboratory20.DesktopApp/Model/POCO/User.cs
+++ MedicalLaboratory20.DesktopApp/Model/POCO/User.cs
@@ -1,8 +1,22 @@
-﻿namespace MedicalLaboratory20.DesktopApp.Model.POCO
+﻿using System.Text.Json.Serialization;
+
+namespace MedicalLaboratory20.DesktopApp.Model.POCO
 {
     internal class User
     {
+        [JsonPropertyName("login")]
+        public string Login { get; set; }
+
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+
+        [JsonPropertyName("role")]
         public string Role { get; set; }
+
+        [JsonPropertyName("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("refreshToken")]
+        public string RefreshToken { get; set; }
     }
 }
--- MedicalLaboratory20.DesktopApp/Services/ApiServices/AuthorizateService.cs
+++ MedicalLaboratory20.DesktopApp/Services/ApiServices/AuthorizateService.cs
@@ -9,7 +9,7 @@ namespace MedicalLaboratory20.DesktopApp.Services.ApiServices
         public IRestResponse Authorizate(string login, string password)
         {
             var cfg = ConfigurationService.GetInstance();
-            IRestRequest request = new RestRequest($"{cfg.HttpServerAddress}/auth/token", Method.POST, DataFormat.Json)
+            IRestRequest request = new RestRequest($"{cfg.HttpServerAddress}/auth/login", Method.POST, DataFormat.Json)
                 .AddJsonBody(new { login, password });
 
             var response = _client.Execute(request);
